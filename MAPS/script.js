86c0a193312d3a2d0a55592163cdac90415bcbb2
EA - Gabrielly Da Frota Ferreira Vaz <gabrielly_ferreira@educadventista.org>
2025-10-21 21:15:33 -0300

    states

--- MAPS/script.js
+++ MAPS/script.js
@@ -1,25 +1,3 @@
-var map = L.map('mapID');
-var boundsSaoPauloEstado = [
-  [-25.5, -53.1],
-  [-19.9, -44.0]  
-];
-
-map.fitBounds(boundsSaoPauloEstado);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-L.marker([20.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
-
-L.marker([-22.0, -47]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
-
-
 var map = L.map('mapCH');
 var boundsSaoPauloEstado = [
   [-25.5, -53.1],
